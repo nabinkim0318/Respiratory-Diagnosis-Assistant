e1656c075b729c9a3a853c2e0cd4c3a77627b8ee
Nabin Kim <32893645+nabinkim0318@users.noreply.github.com>
2024-04-25 17:25:33 -0400

    Update settings.py

--- respiratory_diagnosis_assistant/respiratory_diagnosis_assistant/settings.py
+++ respiratory_diagnosis_assistant/respiratory_diagnosis_assistant/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from storages.backends.s3boto3 import S3Boto3Storage
+
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
