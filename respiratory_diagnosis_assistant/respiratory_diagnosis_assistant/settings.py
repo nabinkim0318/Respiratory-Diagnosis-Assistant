fd119522f1bc569a20456fa4e190db6e28fa6b44
Nabin Kim <32893645+nabinkim0318@users.noreply.github.com>
2024-04-25 17:23:37 -0400

    Update settings.py

--- respiratory_diagnosis_assistant/respiratory_diagnosis_assistant/settings.py
+++ respiratory_diagnosis_assistant/respiratory_diagnosis_assistant/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-#=j8xw2m9_96m24(&!btkd(9syt)17y!7#tbwji%obilcf#(3y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
 
 
 # Application definition
